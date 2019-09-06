431fa0b0200b76caae0103587fdf4eada2b3707d
ElAvocato <emanuel2000veron@hotmail.com>
2019-09-06 17:22:30 -0300

    juego

---
+++ juego.php
@@ -0,0 +1,28 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style_game.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title></title>
+  </head>
+  <body>
+    <div class="">
+      <div class="container_image">
+        <img src="images_game/fruta/fruta.jpg" class="images">
+      </div>
+
+      <div class="container_word">
+        <form action="" method="post">
+          <label for="palabra">Ingrese palabre</label><br><br>
+          <input type="text" name="palabra" value="" placeholder="ingrese palabra"><br><br>
+
+        </form>
+      </div>
+  </div>
+  </body>
+</html>
